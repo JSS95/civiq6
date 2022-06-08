fa9ec2665f741f7f92d3d30a17e2e2f3a1f32a4c
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-06-08 14:12:49 +0900

    feat: add modules
    Mock PyQt5

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-autodoc_mock_imports = ["vimba"]
+autodoc_mock_imports = ["vimba", "PyQt5",]
 
 
 # -- Project information -----------------------------------------------------
