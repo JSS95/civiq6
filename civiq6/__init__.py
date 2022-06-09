b01a264b00ba67eec0ec30e70aa0e5b04e0d67d0
Jisoo Song <jeesoo9595@snu.ac.kr>
2022-06-09 13:23:51 +0900

    doc: add docstrings
    Modify docstring of __init__.py

--- civiq6/__init__.py
+++ civiq6/__init__.py
@@ -2,7 +2,8 @@
 CIViQ6 - Camera Integration for Vimba and Qt6
 =============================================
 
-CIViQ6 is a package to manage the VimbaPython camera instance with Qt6's design.
+CIViQ6 is a package to manage the VimbaPython camera instance with
+Qt6 Python bindings - :mod:`PyQt6` or :mod:`PySide6`.
 
 """
 
