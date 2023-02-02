b6c38e02ccfbab28e518a45765d3ec3b113a58a9
Jisoo Song <jeesoo9595@snu.ac.kr>
2023-02-02 18:11:02 +0900

    feat: implement capture classes
    Add examples/PySide6/imagecapture.py

---
+++ doc/source/examples/PySide6/imagecapture.py
@@ -0,0 +1,47 @@
+import imageio
+import vimba  # type: ignore[import]
+from PySide6.QtCore import QObject, Signal, Slot
+from civiq6 import VimbaCaptureSession
+from typing import Optional
+
+
+VIMBA_LOGGER = vimba.Log.get_instance()
+
+
+class ImageCapture(QObject):
+    imageSaved = Signal(int, str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._captureSession = None
+        self._id = 0
+
+        self._image = None
+        self._writing = False
+
+    def captureSession(self) -> Optional[VimbaCaptureSession]:
+        return self._captureSession
+
+    def _setCaptureSession(self, captureSession: Optional[VimbaCaptureSession]):
+        self._captureSession = captureSession
+
+    @Slot
+    def captureToFile(self, path: str = "") -> int:
+        if self.captureSession() is None or self._image is None:
+            return -1
+        self._writing = True
+
+        imageio.imwrite(path, self._image)
+        ret_id = self._id
+        self._id += 1
+        self._image = None
+        self._writing = False
+
+        VIMBA_LOGGER.info("Captured %s" % path)
+        self.imageSaved.emit(ret_id, path)
+
+        return ret_id
+
+    def _setFrame(self, frame: vimba.Frame):
+        if not self._writing:
+            self._image = frame.as_opencv_image().copy()
